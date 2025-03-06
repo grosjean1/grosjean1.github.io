fa2589108cd99c7210e8209db276a75695cc17d7
grosjean1 <elise.grosjean2@gmail.com>
2025-03-06 13:38:53 +0100

    Build website

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let slides = document.querySelectorAll(".hero-slide");
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.remove("active");
+      if (i === index) {
+        slide.classList.add("active");
+      }
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  setInterval(nextSlide, 3000); // Change de slide toutes les 3 secondes
+});
